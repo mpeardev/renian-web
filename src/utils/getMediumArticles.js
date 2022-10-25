d58228214bbd0f1a712fa59920dba31b66912f6e
mirkoperamas <mirkoperamas@gmail.com>
2022-10-25 15:30:41 -0500

    update to production

---
+++ src/utils/getMediumArticles.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export const getMediumArticles = () => {
+  const [articles, setArticles] = useState();
+
+  useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const requestOptions = {
+      method: "get",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://v1.nocodeapi.com/mpeardev/medium/UwKSEdWUYheCIOgA",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => {
+        const rsp = JSON.parse(result);
+        setArticles(rsp);
+        console.log(rsp);
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return { articles };
+};
